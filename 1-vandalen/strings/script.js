03af3f0ebcc8402c3543594a1ccac143bc3197ed
Christfoffer Holmgren <christoffer.holmgren@gmail.com>
2013-11-15 15:13:31 +0100

    Getting further now, don't know what happened but now it works converting the upper and lowercase letters correctly

--- 1-vandalen/strings/script.js
+++ 1-vandalen/strings/script.js
@@ -4,20 +4,28 @@ window.onload = function(){
 
 	// I denna funktion ska du skriva koden för att hantera "spelet"
 	var convertString = function(str){
-		var converted;
-		for(key in str.split(''))
-		{
-			converted += key.charCodeAt()
+		function logArrayElements(element, index, array) {
+    		console.log("a[" + index + "] = " + element);
+    		if(element == element.toUpperCase())
+    		{
+    			resultstring += element.toLowerCase();
+    			console.log(element.toLowerCase());
+    		}
+    		else if(element == element.toLowerCase())
+			{		
+    			resultstring += element.toUpperCase();
+				console.log(element.toUpperCase());
+			}
 		}
+
+		var result = str.split('');
+		var resultstring = "";
+		result.forEach(logArrayElements)
+		
 		// Plats för förändring.		
 		// Returnera den konverterade strängen.
 		// Vid fel, kasta ett undantag med ett meddelande till användaren.
-		return str.split('').forEach(function(entry){
-			console.log(alph)
-			console.log(parseInt(entry));
-			console.log(parseInt(entry)+32);
-			return toString(parseInt(entry)+32);
-		});
+		return resultstring;
 
 
 

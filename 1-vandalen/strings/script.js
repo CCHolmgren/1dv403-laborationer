b3d8338732c4c3e82324bfc2671a215e33e90a17
Christfoffer Holmgren <christoffer.holmgren@gmail.com>
2013-11-15 15:32:23 +0100

    Beaufity and some renaming

--- 1-vandalen/strings/script.js
+++ 1-vandalen/strings/script.js
@@ -1,58 +1,54 @@
 "use strict";
 
-window.onload = function(){
+window.onload = function () {
 
-	// I denna funktion ska du skriva koden för att hantera "spelet"
-	var convertString = function(str){
-		function upperToLowerLowerToUpper(element, index, array) {
-    		console.log("a[" + index + "] = " + element);
-    		if(element == element.toUpperCase())
-    		{
-    			resultstring += element.toLowerCase();
-    			console.log(element.toLowerCase());
-    		}
-    		else if(element == element.toLowerCase())
-			{		
-    			resultstring += element.toUpperCase();
-				console.log(element.toUpperCase());
-			}
-		}
-		if(str === "")
-			throw {"message": "Du måste skriva något i textrutan innan det kan konverteras!"};
+    // I denna funktion ska du skriva koden för att hantera "spelet"
+    var convertString = function (str) {
+        if (str === "")
+        	//If the input is empty, we can't convert it, so create the object and throw it
+            throw {
+                "message": "Du måste skriva något i textrutan innan det kan konverteras!"
+            };
+            //This function converts the letters to uppercase and lowercase
+        function upperToLowerLowerToUpper(element, index, array) {
+            if (element == element.toUpperCase()) {
+                resultstring += element.toLowerCase();
+            } else if (element == element.toLowerCase()) {
+                resultstring += element.toUpperCase();
+            }
+        }
+        //Split the input so we can forEach the items later
+        var splitString = str.split('');
+        var resultingstring = "";
 
-		var result = str.split('');
-		var resultstring = "";
-		result.forEach(upperToLowerLowerToUpper)
-		resultstring = resultstring.replace(/[aA]/g, "#");
-		// Plats för förändring.		
-		// Returnera den konverterade strängen.
-		// Vid fel, kasta ett undantag med ett meddelande till användaren.
-		return resultstring;
-		// throw {"message":"NEJ!"};
-	};
-	// ------------------------------------------------------------------------------
+        splitString.forEach(upperToLowerLowerToUpper)
+        resultstring = resultstring.replace(/[aA]/g, "#");
+
+        return resultstring;
+    };
+    // ------------------------------------------------------------------------------
 
 
-	// Kod för att hantera utskrift och inmatning. Denna ska du inte behöva förändra
-	var p = document.querySelector("#value"); // Referens till DOM-noden med id="#value"
-	var input = document.querySelector("#string");
-	var submit = document.querySelector("#send");
+    // Kod för att hantera utskrift och inmatning. Denna ska du inte behöva förändra
+    var p = document.querySelector("#value"); // Referens till DOM-noden med id="#value"
+    var input = document.querySelector("#string");
+    var submit = document.querySelector("#send");
 
-	// Vi kopplar en eventhanterare till formulärets skickaknapp som kör en anonym funktion.
-	submit.addEventListener("click", function(e){
-		e.preventDefault(); // Hindra formuläret från att skickas till servern. Vi hanterar allt på klienten.
+    // Vi kopplar en eventhanterare till formulärets skickaknapp som kör en anonym funktion.
+    submit.addEventListener("click", function (e) {
+        e.preventDefault(); // Hindra formuläret från att skickas till servern. Vi hanterar allt på klienten.
 
-		p.classList.remove( "error");
+        p.classList.remove("error");
 
-		try {
-			var answer = convertString(input.value) // Läser in texten från textrutan och skickar till funktionen "convertString"
-			p.innerHTML = answer;		// Skriver ut texten från arrayen som skapats i funktionen.	
-		} catch (error){
-			p.classList.add( "error"); // Växla CSS-klass, IE10+
-			p.innerHTML = error.message;
-		}
-	
-	});
+        try {
+            var answer = convertString(input.value) // Läser in texten från textrutan och skickar till funktionen "convertString"
+            p.innerHTML = answer; // Skriver ut texten från arrayen som skapats i funktionen.	
+        } catch (error) {
+            p.classList.add("error"); // Växla CSS-klass, IE10+
+            p.innerHTML = error.message;
+        }
+
+    });
 
 
 

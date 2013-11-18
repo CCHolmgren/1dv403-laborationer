a3ce7861dad6ae66024aea613c4dd90c210875c0
Christfoffer Holmgren <christoffer.holmgren@gmail.com>
2013-11-18 14:36:31 +0100

    Finally it worked with localeCompare, you just gotta use it correctly

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -2,11 +2,27 @@
 
 var makePerson = function(persArr){
 	//Pure function
+	var sortGeneric = function(a,b){
+		if(a > b)
+			return 1;
+		if(a < b)
+			return -1;
+		return 0;
+	};
+	var sortLetters = function(a,b){
+		if(a.charCodeAt(0)>b.charCodeAt(0))
+			return 1;
+		if(a.charCodeAt(0)<b.charCodeAt(0))
+			return -1;
+		return 0;
+	}
 	var result = new Object();
 	console.log(JSON.stringify(persArr));
 	var names = Object.keys(persArr).map(function(key){return persArr[key];}).map(function(person){return person.name;});
 	console.log(names)
-	names.sort(function(a,b){return (a.localeCompare(b,'sv'))?1:-1});
+	names.sort(function(a,b){
+		return a.localeCompare(b);
+	});
 	var ages = Object.keys(persArr).map(function(key){return persArr[key];}).map(function(person){return person.age;}).sort(function(a,b){return (a>b)?1:-1});
 	console.log(names);
 	console.log(ages);

5cfe0924beb69536d02132c0f47a7d3f4b375ea7
Christfoffer Holmgren <christoffer.holmgren@gmail.com>
2013-11-18 14:42:30 +0100

    Beautifying the code, and now it should be finished

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -1,23 +1,23 @@
 "use strict";
 
 var makePerson = function(persArr){
-	//Pure function
-	var result = {};
-	var names = persArr.map(function(person){
-		return person.name;
-	});
-	console.log(names)
+	var result = {},
+		names = persArr.map(function(person){
+					return person.name;
+				}),
+		ages = persArr.map(function(person){
+					return person.age;
+				}).sort(function(a,b){return (a>b)?1:-1});
+
 	names.sort(function(a,b){
 		return a.localeCompare(b);
 	});
-	var ages = persArr.map(function(person){
-		return person.age;
-	}).sort(function(a,b){return (a>b)?1:-1});
 	result.minAge = ages[0];
 	result.maxAge = ages[ages.length-1];
 	result.averageAge = Math.round(ages.reduce(function(a,b){
-		return a+b;
-	})/ages.length);
+							return a+b;
+						})/ages.length);
 	result.names = names.join(", ");
+
 	return result;
 }

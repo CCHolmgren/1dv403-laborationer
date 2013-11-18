ba8c0c2b5c9c32de6fc03a445f8d1f58b11b241e
Christfoffer Holmgren <christoffer.holmgren@gmail.com>
2013-11-18 14:39:12 +0100

    Fixing up the code and removing some unneccessary code

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -2,35 +2,22 @@
 
 var makePerson = function(persArr){
 	//Pure function
-	var sortGeneric = function(a,b){
-		if(a > b)
-			return 1;
-		if(a < b)
-			return -1;
-		return 0;
-	};
-	var sortLetters = function(a,b){
-		if(a.charCodeAt(0)>b.charCodeAt(0))
-			return 1;
-		if(a.charCodeAt(0)<b.charCodeAt(0))
-			return -1;
-		return 0;
-	}
-	var result = new Object();
-	console.log(JSON.stringify(persArr));
-	var names = Object.keys(persArr).map(function(key){return persArr[key];}).map(function(person){return person.name;});
+	var result = {};
+	var names = persArr.map(function(person){
+		return person.name;
+	});
 	console.log(names)
 	names.sort(function(a,b){
 		return a.localeCompare(b);
 	});
-	var ages = Object.keys(persArr).map(function(key){return persArr[key];}).map(function(person){return person.age;}).sort(function(a,b){return (a>b)?1:-1});
-	console.log(names);
-	console.log(ages);
-	// var arr = Object.keys( persArr ).map(function ( key ) { return persArr[key]; });
-	// console.log(arr);
+	var ages = persArr.map(function(person){
+		return person.age;
+	}).sort(function(a,b){return (a>b)?1:-1});
 	result.minAge = ages[0];
 	result.maxAge = ages[ages.length-1];
-	result.averageAge = Math.round(ages.reduce(function(a,b){return a+b})/ages.length);
+	result.averageAge = Math.round(ages.reduce(function(a,b){
+		return a+b;
+	})/ages.length);
 	result.names = names.join(", ");
 	return result;
 }

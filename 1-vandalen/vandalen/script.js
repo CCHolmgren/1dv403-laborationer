51526b92d605db3400c764995d57e562b771c53a
Christfoffer Holmgren <christoffer.holmgren@gmail.com>
2013-11-18 12:56:22 +0100

    More or less the whole solution. Gotta make it look a lot better first though

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -1,9 +1,21 @@
 "use strict";
 
 var makePerson = function(persArr){
-
-
-	// Din kod här...
-
+	//Pure function
+	var result = {};
+	var names = Object.keys(data).map(function(key){return data[key];}).map(function(person){return person.name;}).sort(function(a,b){return (a>b)?1:-1});
+	var ages = Object.keys(data).map(function(key){return data[key];}).map(function(person){return person.age;}).sort(function(a,b){return (a>b)?1:-1});
+	var arr = Object.keys( persArr ).map(function ( key ) { return persArr[key]; });
+	console.log(arr);
+	result.minAge = ages[0];
+	result.maxAge = ages[ages.length-1];
+	result.averageAge = Math.round(ages.reduce(function(a,b){return a+b})/ages.length);
+	result.names = names.join(", ");
+	return result;
 }
 
+var data = [{name: "John Häggerud", age: 37}, {name: "Johan Leitet", age: 36}, {name: "Mats Loock", age: 46}];
+
+var result = makePerson(data);
+
+console.log(result);

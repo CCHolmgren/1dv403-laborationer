984b29893128573d9e8035df4f350cda32e246fe
Christfoffer Holmgren <christoffer.holmgren@gmail.com>
2013-11-26 09:56:01 +0100

    Initial

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -1,23 +1,25 @@
 "use strict";
 
-var makePerson = function(persArr){
-	var result = {},
-		names = persArr.map(function(person){
-					return person.name;
-				}),
-		ages = persArr.map(function(person){
-					return person.age;
-				}).sort(function(a,b){return (a>b)?1:-1});
+var makePerson = function (persArr) {
+    var result = {},
+        names = persArr.map(function (person) {
+            return person.name;
+        }),
+        ages = persArr.map(function (person) {
+            return person.age;
+        }).sort(function (a, b) {
+            return (a > b) ? 1 : -1
+        });
 
-	names.sort(function(a,b){
-		return a.localeCompare(b);
-	});
-	result.minAge = ages[0];
-	result.maxAge = ages[ages.length-1];
-	result.averageAge = Math.round(ages.reduce(function(a,b){
-							return a+b;
-						})/ages.length);
-	result.names = names.join(", ");
+    names.sort(function (a, b) {
+        return a.localeCompare(b);
+    });
+    result.minAge = ages[0];
+    result.maxAge = ages[ages.length - 1];
+    result.averageAge = Math.round(ages.reduce(function (a, b) {
+        return a + b;
+    }) / ages.length);
+    result.names = names.join(", ");
 
-	return result;
+    return result;
 }

1077dfef07dae70093ed6b2ff9d90b03015ff4c2
Christfoffer Holmgren <christoffer.holmgren@gmail.com>
2013-11-26 11:32:48 +0100

    Adding some ids so they can be targeted with the querySelector

--- 2-labbymezzage/script.js
+++ 2-labbymezzage/script.js
@@ -3,4 +3,10 @@
 window.onload = function(){
 	MessageBoard.init("Textmeddelande");
 	alert(MessageBoard.messages[0].getText());
+
+	var button = document.querySelector("#sendbutton");
+	button.addEventListener("click", function(e){
+		e.preventDefault();
+		MessageBoard.init(document.querySelector("#inputarea").value);
+	});
 }

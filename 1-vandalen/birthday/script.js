7be60a4d41cbdc4d52330d6a21084139ee81ab83
Christfoffer Holmgren <christoffer.holmgren@gmail.com>
2013-11-28 23:55:19 +0100

    Some formatting, comments and a bugfix that I managed to sneak in there while I was fixing

--- 1-vandalen/birthday/script.js
+++ 1-vandalen/birthday/script.js
@@ -5,29 +5,38 @@ window.onload = function(){
 	
 	var birthday = function(date){
 		console.log(date);
+		
 		//Matches things on the format [0-9]*4-[0-9][1-9]-[0-9][1-9]
 		//Which is what we assume the input should be like
-		var re = /\d{4,4}-\d{2,2}-\d{2,2}/;
+		var re = /\d{4,4}-\d{2,2}-\d{2,2}/,
+			today = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate()),
+			millisecondsToDays = 24*60*60*1000,
+			birthday = date.split('-'); //Split into the specific parts, assuming YYYY-MM-DD format
+
 		if(!date.match(re))
 			throw {'message':'Du måste fylla i ett giltigt datum på formatet YYYY-MM-DD!'};
 
-		var today = Date.now(),
-			millisecondsToDays = 24*60*60*1000,
-			birthday = date.split('-'); //Split into the specific parts, assuming YYYY-MM-DD format
 		//Remove the first object since we don't care what year they were born
 		birthday.splice(0,1); 
+
 		 //Assume the birth day is this year
 		var nextBirthday = new Date(new Date().getFullYear(), birthday[0]-1, birthday[1]);
-		 console.log("Nextbirthday: " + nextBirthday);
-		//If it's not, add one to the year
-		console.log("Todays date: " + new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate()));
-		console.log("Comparison: " + (nextBirthday < new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate())));
-		if(nextBirthday < new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate()))
+
+		console.log("Nextbirthday: " + nextBirthday);
+		console.log("Todays date: " + today);
+		console.log("Comparison: " + (nextBirthday < today));
+
+		//If today is larger than nextBirthday, then it must be because it's already happened
+		if(nextBirthday < today)
+			//So we add a year to it, and it should be fine.
 			nextBirthday = new Date(new Date().getFullYear()+1, birthday[0]-1, birthday[1]);
+
 		console.log("nextBirthday: " + nextBirthday);
+
 		//Math.round will round up or down,
 		//Math abs will turn the negative number we get from the subtraction(could probably just switch them though)
 		//Then convert the milliseconds to days via the divison of milliseconds in days
+		console.log(Math.round(Math.abs((today-nextBirthday)/millisecondsToDays)));
 		return Math.round(Math.abs((today - nextBirthday)/millisecondsToDays));
 		};
 	// ------------------------------------------------------------------------------

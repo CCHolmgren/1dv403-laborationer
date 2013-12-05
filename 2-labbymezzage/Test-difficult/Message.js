90d34a8b2371d5b8419375a2ac0d89b0ecb758e5
Christfoffer Holmgren <christoffer.holmgren@gmail.com>
2013-12-05 23:47:53 +0100

    function style 'use strict' seems to work just as it should

--- 2-labbymezzage/Test-difficult/Message.js
+++ 2-labbymezzage/Test-difficult/Message.js
@@ -1,62 +1,63 @@
-"use strict";
-
-function Message(inputmessage, inputdate) {
-    var message;
-    var date;
-    this.id = Math.floor(Math.random() * Math.pow(2, 64));
-
-    this.getText = function () {
-        return message;
-    };
-
-    this.setText = function (_text) {
-        message = _text;
-    };
-
-    this.getDate = function () {
-        return date;
-    };
-
-    this.setDate = function (_date) {
-        date = _date;
-    };
-
-    this.setText(inputmessage);
-    this.setDate(inputdate);
-}
-
-Message.prototype.toString = function () {
-    return this.getText() + " (" + this.getDate() + ")";
-};
-
-Message.prototype.getHTMLText = function () {
-    return this.getText().escapelHTML().replace(/[\n\r]/g, "<br />");
-};
-
-Message.prototype.getDateText = function () {
-    var date = this.getDate();
-    var hour = date.getHours();
-    var min = date.getMinutes();
-    var sec = date.getSeconds();
-    min += "";
-    sec += "";
-
-    if (min.length == 1) {
-        min = "0" + min;
+(function(){
+    "use strict";
+    
+    function Message(inputmessage, inputdate) {
+        var message;
+        var date;
+        this.id = Math.floor(Math.random() * Math.pow(2, 64));
+    
+        this.getText = function () {
+            return message;
+        };
+    
+        this.setText = function (_text) {
+            message = _text;
+        };
+    
+        this.getDate = function () {
+            return date;
+        };
+    
+        this.setDate = function (_date) {
+            date = _date;
+        };
+    
+        this.setText(inputmessage);
+        this.setDate(inputdate);
     }
-    if (sec.length == 1) {
-        sec = "0" + sec;
-    }
-
-    return hour + ":" + min + ":" + sec;
-};
-
-String.prototype.escapelHTML = function () {
-    return String(this)
-        .replace(/&/g, "&amp;")
-        .replace(/</g, "&lt;")
-        .replace(/>/g, "&gt;")
-        .replace(/"/g, "&quot;")
-        .replace(/'/g, "&#039;")
-        .replace(/\//g, "&#x2F;");
-};
+    
+    Message.prototype.toString = function () {
+        return this.getText() + " (" + this.getDate() + ")";
+    };
+    
+    Message.prototype.getHTMLText = function () {
+        return this.getText().escapelHTML().replace(/[\n\r]/g, "<br />");
+    };
+    
+    Message.prototype.getDateText = function () {
+        var date = this.getDate();
+        var hour = date.getHours();
+        var min = date.getMinutes();
+        var sec = date.getSeconds();
+        min += "";
+        sec += "";
+    
+        if (min.length == 1) {
+            min = "0" + min;
+        }
+        if (sec.length == 1) {
+            sec = "0" + sec;
+        }
+    
+        return hour + ":" + min + ":" + sec;
+    };
+    
+    String.prototype.escapelHTML = function () {
+        return String(this)
+            .replace(/&/g, "&amp;")
+            .replace(/</g, "&lt;")
+            .replace(/>/g, "&gt;")
+            .replace(/"/g, "&quot;")
+            .replace(/'/g, "&#039;")
+            .replace(/\//g, "&#x2F;");
+    };})();
--- 2-labbymezzage/Test-difficult/script.js
+++ 2-labbymezzage/Test-difficult/script.js
@@ -1,7 +1,7 @@
-"use strict";
+(function(){"use strict";
 
 window.onload = function () {
     new MessageBoard("div1");
     new MessageBoard("div2");
     new MessageBoard("div3");
-};
+};})();

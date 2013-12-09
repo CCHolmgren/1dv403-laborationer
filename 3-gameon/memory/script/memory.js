db160ec0c06c55b442c9130cb1daf38c483db187
Christfoffer Holmgren <christoffer.holmgren@gmail.com>
2013-12-09 15:14:26 +0100

    Changed so that it would apply row divs but its not fully working yet

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -5,9 +5,11 @@ function Memory(div, inputRows, inputCols) {
     var rows = inputRows;
     var cols = inputCols;
     var selecteddiv = document.getElementById(div);
+
     this.init = function () {
         memory = RandomGenerator.getPictureArray(rows, cols);
     };
+
     this.start = function () {
         var imagesFlipped = 0;
         var clickedImage = "";
@@ -15,7 +17,62 @@ function Memory(div, inputRows, inputCols) {
         var stopFlipping = false;
         var guesses = 0;
         var won = false;
-        memory.forEach(function (element) {
+        for (var i = 0; i < rows; i++) {
+            var div = document.createElement("div");
+
+            for (var j = 0; j < cols; j++) {
+                var a = document.createElement("a");
+                var image = document.createElement("img");
+                var that = this;
+
+                image.setAttribute("data-src", "pics/" + memory[i*j] + ".png");
+                image.setAttribute("src", "pics/0.png");
+
+                a.appendChild(image);
+                a.setAttribute("href", "#");
+                a.addEventListener("click", function (e) {
+                    if (!won) {
+                        if (imagesFlipped === 0 && !stopFlipping) {
+                            lastClickedImage = e.target;
+                            imagesFlipped += 1;
+                            guesses += 1;
+                        }
+                        if (imagesFlipped < 2 && !stopFlipping) {
+                            e.target.setAttribute("src", e.target.dataset.src);
+                            if (lastClickedImage != e.target) {
+                                imagesFlipped += 1;
+                                guesses += 1;
+                            }
+                        }
+                        if (imagesFlipped == 2 && !stopFlipping) {
+                            stopFlipping = true;
+                            if (lastClickedImage != e.target && lastClickedImage.getAttribute("src") == e.target.getAttribute("src")) {
+                                var win = document.getElementById("win");
+                                var text = document.createTextNode("Du vann. Det krävdes " + guesses + " gissningar för dig att hitta de matchande bilderna!");
+                                win.appendChild(text);
+                                won = true;
+                                var images = document.querySelectorAll("a img");
+                                for (var i = 0; i < images.length; i++) {
+                                    images[i].setAttribute("src", images[i].dataset.src);
+                                }
+                            }
+                            if (!won) {
+                                setTimeout(function () {
+                                    lastClickedImage.setAttribute("src", "pics/0.png");
+                                    e.target.setAttribute("src", "pics/0.png");
+                                    stopFlipping = false;
+                                    imagesFlipped = 0;
+                                }, 1000);
+                            }
+                        }
+                    }
+                });
+                div.appendChild(a);
+            }
+            selecteddiv.appendChild(div);
+        }
+        
+        /*memory.forEach(function (element) {
             var div = document.createElement("div");
             var a = document.createElement("a");
             var image = document.createElement("img");
@@ -48,23 +105,23 @@ function Memory(div, inputRows, inputCols) {
                             win.appendChild(text);
                             won = true;
                             var images = document.querySelectorAll("a img");
-                            for(var i = 0; i < images.length; i++){
+                            for (var i = 0; i < images.length; i++) {
                                 images[i].setAttribute("src", images[i].dataset.src);
                             }
                         }
-                        if(!won){
-                        setTimeout(function () {
-                            lastClickedImage.setAttribute("src", "pics/0.png");
-                            e.target.setAttribute("src", "pics/0.png");
-                            stopFlipping = false;
-                            imagesFlipped = 0;
-                        }, 1000);
-                    }}
+                        if (!won) {
+                            setTimeout(function () {
+                                lastClickedImage.setAttribute("src", "pics/0.png");
+                                e.target.setAttribute("src", "pics/0.png");
+                                stopFlipping = false;
+                                imagesFlipped = 0;
+                            }, 1000);
+                        }
+                    }
                 }
             });
-
             div.appendChild(a);
             selecteddiv.appendChild(div);
-        });
+        });*/
     };
 }

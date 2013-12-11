9d09a5cf3e26f5f4a5eb49d42af80dda557da9e5
Christfoffer Holmgren <christoffer.holmgren@gmail.com>
2013-12-11 15:18:54 +0100

    Beautify some more

--- 3-gameon/memory/script/script.js
+++ 3-gameon/memory/script/script.js
@@ -1,10 +1,10 @@
 "use strict";
 
 window.onload = function () {
-    var mem = new Memory("game1","win1" , 2, 8);
+    var mem = new Memory("game1", "win1", 2, 8);
     mem.init();
     mem.start();
-    var gaem = new Memory("game2","win2" , 4, 4);
+    var gaem = new Memory("game2", "win2", 4, 4);
     gaem.init();
     gaem.start();
 };

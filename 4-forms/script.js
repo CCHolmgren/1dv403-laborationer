bca64a0224419b17aff7fc74908d287001222de8
Christfoffer Holmgren <christoffer.holmgren@gmail.com>
2013-12-16 13:36:25 +0100

    The classes will not just be appended, they will be reset instead

--- 4-forms/script.js
+++ 4-forms/script.js
@@ -11,9 +11,20 @@ function Validator() {
         this.nonempty[i].addEventListener("change", function (e) {
             console.log(e);
             if (that.validateNonEmpty(e.target)) {
-                e.target.parentNode.className += " has-success";
+                e.target.parentNode.className = e.target.parentNode.dataset.class + " has-success";
             } else {
-                e.target.parentNode.className += " has-warning";
+                e.target.parentNode.className = e.target.parentNode.dataset.class + " has-error";
+            }
+
+        });
+    }
+    for (i = 0; i < this.nonempty.length; i++) {
+        this.nonempty[i].addEventListener("blur", function (e) {
+            console.log(e);
+            if (that.validateNonEmpty(e.target)) {
+                e.target.parentNode.className = e.target.parentNode.dataset.class + " has-success";
+            } else {
+                e.target.parentNode.className = e.target.parentNode.dataset.class + " has-error";
             }
 
         });
@@ -22,9 +33,9 @@ function Validator() {
         this.postcode[i].addEventListener("change", function (e) {
             console.log(e);
             if (that.validateSwePostCode(e.target)) {
-                e.target.parentNode.className += " has-success";
+                e.target.parentNode.className = e.target.parentNode.dataset.class + " has-success";
             } else {
-                e.target.parentNode.className += " has-error";
+                e.target.parentNode.className = e.target.parentNode.dataset.class + " has-error";
             }
         });
     }
@@ -32,22 +43,23 @@ function Validator() {
         this.email[i].addEventListener("change", function(e){
             console.log(e);
             if(that.validateEmail(e.target)){
-                e.target.parentNode.className += " has-success";
+                e.target.parentNode.className = e.target.parentNode.dataset.class + " has-success";
             }
             else{
-                e.target.parentNode.className += " has-error";
+                e.target.parentNode.className = e.target.parentNode.dataset.class + " has-success";
             }
         });
     }
     this.validateNonEmpty = function (e) {
-        return e.value != String.empty;
+        console.log(e.value);
+        return e.value !== "";
     };
     this.validateSwePostCode = function (e) {
         var re = /^(((SE |SE)?\d{5})|((SE |SE)?\d{3}(?: )\d{2})|((SE |SE)?\d{3}(?:-)\d{2}))$/;
         return re.test(e.value);
     };
     this.validateEmail = function(e){
-        var re = /.*@.*/;
+        var re = /.+@.+/;
         return re.test(e.value);
     };
 }

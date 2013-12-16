ea7bbbf599cf549e6592af1da89d02c6f16107e4
Christfoffer Holmgren <christoffer.holmgren@gmail.com>
2013-12-16 11:18:00 +0100

    Trying to get bootstrap to highlight the inputs correctly, but it doesn't work yet

--- 4-forms/script.js
+++ 4-forms/script.js
@@ -1,3 +1,22 @@
 function Validator(){
-    
+    this.elements = document.querySelectorAll("input[type=text]");
+    this.nonempty = document.getElementsByClassName("non-empty");
+    var that = this;
+    for(var i = 0; i < this.nonempty.length; i++)
+    {
+        this.nonempty[i].addEventListener("change", function(e){
+            console.log(e);
+            if(that.validateNonEmpty(e.target)){
+                e.target.parentNode.className += " has-success";
+            }
+            else{
+                e.target.parentNode.className += " has-warning";
+            }
+            
+        });
+    }
+    console.log(this.elements);
+    this.validateNonEmpty = function(e){
+        return e.value != "";
+    };
 }

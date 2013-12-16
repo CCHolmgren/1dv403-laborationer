b3f482325460877695172c0627479e8ffa251d96
Christfoffer Holmgren <christoffer.holmgren@gmail.com>
2013-12-16 11:40:31 +0100

    Added some validators

--- 4-forms/script.js
+++ 4-forms/script.js
@@ -1,22 +1,48 @@
-function Validator(){
+/* jshint strict: true */
+/* global document, console */
+function Validator() {
+    "use strict";
     this.elements = document.querySelectorAll("input[type=text]");
     this.nonempty = document.getElementsByClassName("non-empty");
+    this.postcode = document.getElementsByClassName("postcode");
+    this.email = document.getElementsByClassName("validemail");
     var that = this;
-    for(var i = 0; i < this.nonempty.length; i++)
-    {
-        this.nonempty[i].addEventListener("change", function(e){
+    for (var i = 0; i < this.nonempty.length; i++) {
+        this.nonempty[i].addEventListener("change", function (e) {
             console.log(e);
-            if(that.validateNonEmpty(e.target)){
+            if (that.validateNonEmpty(e.target)) {
                 e.target.parentNode.className += " has-success";
-            }
-            else{
+            } else {
                 e.target.parentNode.className += " has-warning";
             }
-            
+
         });
     }
+    for (i = 0; i < this.postcode.length; i++) {
+        this.postcode[i].addEventListener("change", function (e) {
+            console.log(e);
+            if (that.validateSwePostCode(e.target)) {
+                e.target.parentNode.className += " has-success";
+            } else {
+                e.target.parentNode.className += " has-error";
+            }
+        });
+    }
+    for(i = 0; i < this.email.length; i++){
+        this.email[i].addEventListener("change", function(e){
+            console.log(e);
+        })
+    }
     console.log(this.elements);
-    this.validateNonEmpty = function(e){
-        return e.value != "";
+    this.validateNonEmpty = function (e) {
+        return e.value != String.empty;
     };
+    this.validateSwePostCode = function (e) {
+        var re = /\d{5}/;
+        return re.test(e.value);
+    };
+    this.validateEmail = function(e){
+        var re = /.*@.*/;
+        return re.test(e.value);
+    }
 }

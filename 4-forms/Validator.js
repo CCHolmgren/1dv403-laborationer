f6fa3c9e34c83db88f814d486b97ad02d5868cb2
Christfoffer Holmgren <christoffer.holmgren@gmail.com>
2013-12-18 13:15:21 +0100

    Added so that you much have enetered input into all the inputs before continuing

--- 4-forms/Validator.js
+++ 4-forms/Validator.js
@@ -55,7 +55,7 @@ function Validator() {
         this.nonempty[i].addEventListener("change", this.inputValidationHandler("validateNonEmpty", false));
         this.nonempty[i].addEventListener("blur", this.inputValidationHandler("validateNonEmpty", false));
     }
-    
+
     for (i = 0; i < this.postcode.length; i++) {
         this.postcode[i].parentNode.dataset.class = this.postcode[i].parentNode.className;
         this.postcode[i].addEventListener("change", function (e) {
@@ -67,31 +67,39 @@ function Validator() {
             e.target.value = that.validPostCode(e.target);
         });
     }
-    
+
     for (i = 0; i < this.email.length; i++) {
         this.email[i].parentNode.dataset.class = this.email[i].parentNode.className;
         this.email[i].addEventListener("change", this.inputValidationHandler("validateEmail", false));
     }
-    
+
     this.button.addEventListener("click", function (e) {
         e.preventDefault();
-        var modal = new Modal(),
-            labels = document.getElementsByClassName("use-label"),
-            inputs = document.getElementsByClassName("use-input"),
-            modalInformation = {};
-        modalInformation.categories = [];
-        modalInformation.values = [];
-        for (var i = 0; i < labels.length; i++) {
-            if (inputs[i].nodeName === "SELECT") {
-                modalInformation.values.push(inputs[i].options[inputs[i].selectedIndex].value);
-            } else {
-                modalInformation.values.push(inputs[i].value);
+        NodeList.prototype.every = Array.prototype.every;
+        var i,
+            allValidated = that.elements.every(function(element){
+                console.log(element);
+                return element.parentNode.classList.contains("has-success");
+            });
+        if (allValidated) {
+            var modal = new Modal(),
+                labels = document.getElementsByClassName("use-label"),
+                inputs = document.getElementsByClassName("use-input"),
+                modalInformation = {};
+            modalInformation.categories = [];
+            modalInformation.values = [];
+            for (i = 0; i < labels.length; i++) {
+                if (inputs[i].nodeName === "SELECT") {
+                    modalInformation.values.push(inputs[i].options[inputs[i].selectedIndex].value);
+                } else {
+                    modalInformation.values.push(inputs[i].value);
+                }
+                console.log("values: " + modalInformation.values[i]);
+                modalInformation.categories.push(labels[i].firstChild);
+                console.log("categories: " + modalInformation.categories[i]);
             }
-            console.log("values: " + modalInformation.values[i]);
-            modalInformation.categories.push(labels[i].firstChild);
-            console.log("categories: " + modalInformation.categories[i]);
+            console.log(modalInformation);
+            modal.Open(modalInformation);
         }
-        console.log(modalInformation);
-        modal.Open(modalInformation);
     });
 }

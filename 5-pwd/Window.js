5e73e76bea08e1a142c499727fed96bc0b44712d
Christfoffer Holmgren <christoffer.holmgren@gmail.com>
2013-12-21 11:22:20 +0100

    A simple item with size

--- 5-pwd/Window.js
+++ 5-pwd/Window.js
@@ -1,5 +1,15 @@
-/* global window */
+/* global window, console */
 window.JAWM = {};
-window.JAWM.Window = (function(){
+window.JAWM.Window = function(){
     "use strict";
-}());
+    var _height;
+    var _width;
+    this.setSize = function(height, width){
+        this._height = height;
+        this._width = width;
+    };
+    this.printSize = function(){
+        console.log(this._height);
+        console.log(this._width);
+    };
+};
--- 5-pwd/script.js
+++ 5-pwd/script.js
@@ -1,4 +1,7 @@
 /* global window */
 window.onload = function (){
     "use strict";
+    var testWindow = new window.JAWM.Window();
+    testWindow.setSize(100, 100);
+    testWindow.printSize();
 };

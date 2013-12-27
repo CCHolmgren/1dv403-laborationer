1e92a4ce8d6705c3b36e50c8944fae9c65278937
Christfoffer Holmgren <christoffer.holmgren@gmail.com>
2013-12-27 11:59:21 +0100

    IIFE to stop JSHint from complaining

--- 5-pwd/ImageViewer.js
+++ 5-pwd/ImageViewer.js
@@ -1,77 +1,79 @@
 /* global JAWM, console, document, XMLHttpRequest */
-"use strict";
+(function () {
+    "use strict";
 
-JAWM.ImageViewer = function (id, top, left) {
-    console.log("Imageviewer");
-    this._id = id;
-    this._top = top;
-    this._left = left;
-    this._loadedimages = null;
-};
-
-JAWM.ImageViewer.prototype = new JAWM.Window();
-JAWM.ImageViewer.prototype.content = function () {
-    var div = document.createElement("div");
-    this.xhrgetimages(div);
-    return div;
-};
-
-JAWM.ImageViewer.prototype.xhrgetimages = function (divhandle) {
-    var xhr = new XMLHttpRequest(),
-        that = this,
-        handle = divhandle;
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState == 4) {
-            var response = JSON.parse(xhr.responseText),
-                largestimages = that.largestimage(response);
-            for (var i = 0; i < response.length; i++) {
-                var img = document.createElement("img"),
-                    div = document.createElement("div");
-                img.setAttribute("src", response[i].thumbURL);
-                img.setAttribute("data-imgvalue", i);
-                div.style.width = largestimages[0] + "px";
-                div.style.height = largestimages[1] + "px";
-                div.style.float = "left";
-                div.addEventListener("click", function (e) {
-                    console.log(e);
-                    console.log("Response inside click event", response);
-                    document.body.style.backgroundImage = 'url(' + response[(e.toElement.localName == "div" )? e.target.firstChild.dataset.imgvalue: e.target.dataset.imgvalue].URL + ')';
-                });
-                div.appendChild(img);
-                handle.appendChild(div);
-                handle.style.position = "relative";
-                handle.style.top = "0";
-                handle.style.left = "0";
-                handle.style.overflowY = "scroll";
-                handle.style.height = "calc(100% - 78px)";
-                //handle.style.height = "calc(100%-26px)";
-            }
-        }
+    JAWM.ImageViewer = function (id, top, left) {
+        console.log("Imageviewer");
+        this._id = id;
+        this._top = top;
+        this._left = left;
+        this._loadedimages = null;
     };
-    xhr.open('GET', 'http://homepage.lnu.se/staff/tstjo/labbyServer/imgviewer/?' + this.getID(), true);
-    xhr.send(null);
-};
 
-JAWM.ImageViewer.prototype.largestimage = function (listofimages) {
-    var maxHeight = 0,
-        maxWidth = 0,
-        keys = Object.keys(listofimages),
-        imageWidths = keys.map(function (key) {
-            return listofimages[key].thumbWidth;
-        }),
-        imageHeights = keys.map(function (key) {
-            return listofimages[key].thumbHeight;
+    JAWM.ImageViewer.prototype = new JAWM.Window();
+    JAWM.ImageViewer.prototype.content = function () {
+        var div = document.createElement("div");
+        this.xhrgetimages(div);
+        return div;
+    };
+
+    JAWM.ImageViewer.prototype.xhrgetimages = function (divhandle) {
+        var xhr = new XMLHttpRequest(),
+            that = this,
+            handle = divhandle;
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == 4) {
+                var response = JSON.parse(xhr.responseText),
+                    largestimages = that.largestimage(response);
+                for (var i = 0; i < response.length; i++) {
+                    var img = document.createElement("img"),
+                        div = document.createElement("div");
+                    img.setAttribute("src", response[i].thumbURL);
+                    img.setAttribute("data-imgvalue", i);
+                    div.style.width = largestimages[0] + "px";
+                    div.style.height = largestimages[1] + "px";
+                    div.style.float = "left";
+                    div.addEventListener("click", function (e) {
+                        console.log(e);
+                        console.log("Response inside click event", response);
+                        document.body.style.backgroundImage = 'url(' + response[(e.toElement.localName == "div") ? e.target.firstChild.dataset.imgvalue : e.target.dataset.imgvalue].URL + ')';
+                    });
+                    div.appendChild(img);
+                    handle.appendChild(div);
+                    handle.style.position = "relative";
+                    handle.style.top = "0";
+                    handle.style.left = "0";
+                    handle.style.overflowY = "scroll";
+                    handle.style.height = "calc(100% - 78px)";
+                    //handle.style.height = "calc(100%-26px)";
+                }
+            }
+        };
+        xhr.open('GET', 'http://homepage.lnu.se/staff/tstjo/labbyServer/imgviewer/?' + this.getID(), true);
+        xhr.send(null);
+    };
+
+    JAWM.ImageViewer.prototype.largestimage = function (listofimages) {
+        var maxHeight = 0,
+            maxWidth = 0,
+            keys = Object.keys(listofimages),
+            imageWidths = keys.map(function (key) {
+                return listofimages[key].thumbWidth;
+            }),
+            imageHeights = keys.map(function (key) {
+                return listofimages[key].thumbHeight;
+            });
+        imageHeights.forEach(function (element) {
+            if (element > maxHeight) {
+                maxHeight = element;
+            }
         });
-    imageHeights.forEach(function (element) {
-        if (element > maxHeight) {
-            maxHeight = element;
-        }
-    });
-    imageWidths.forEach(function (element) {
-        if (element > maxWidth) {
-            maxWidth = element;
-        }
-    });
-    console.log(maxHeight, maxWidth);
-    return [maxWidth, maxHeight];
-};
+        imageWidths.forEach(function (element) {
+            if (element > maxWidth) {
+                maxWidth = element;
+            }
+        });
+        console.log(maxHeight, maxWidth);
+        return [maxWidth, maxHeight];
+    };
+}());
--- 5-pwd/Window.js
+++ 5-pwd/Window.js
@@ -1,115 +1,117 @@
 /* global window, console, document, JAWM */
-window.JAWM = {};
-JAWM.Window = function Window(id, top, left) {
-    "use strict";
-    this._height = 0;
-    this._width = 0;
-    this._id = id;
-    this._dom = null;
-    this._window = null;
-    this._zindex = 0;
-    this._topbar = null;
-    this._top = top || 1;
-    this._left = left || 1;
-    this._content = null;
+(function () {
+    window.JAWM = {};
+    JAWM.Window = function Window(id, top, left) {
+        "use strict";
+        this._height = 0;
+        this._width = 0;
+        this._id = id;
+        this._dom = null;
+        this._window = null;
+        this._zindex = 0;
+        this._topbar = null;
+        this._top = top || 1;
+        this._left = left || 1;
+        this._content = null;
 
-    console.log("id ", id);
-    console.log("_id ", this._id);
-};
-var Window = JAWM.Window;
-Window.prototype.setSize = function (width, height) {
-    console.log("Inside setSize");
-    this._dom.style.height = height + "px";
-    this._dom.style.width = width + "px";
-    console.log(this._dom);
-    this._height = height;
-    this._width = width;
-    return this;
-};
-Window.prototype.content = function(){
-    var div = document.createElement("div");
-    div.classList.add("content");
-    return div;
-};
-Window.prototype.getWindow = function () {
-    return this._window;
-};
-Window.prototype.getTopbar = function () {
-    return this._topbar;
-};
-Window.prototype.getID = function () {
-    return this._id;
-};
-Window.prototype.getSize = function () {
-    return [this._width, this._height];
-};
-Window.prototype.render = function () {
-    this._window = document.createElement("div");
-    this._window.classList.add("icon");
-    this.setzIndex(this.getID());
-    //this._window.style.overflowY = "scroll";
-    this._window.style.width = this._width + "px";
-    //this._window.style.height = this._height + "px";
-    this._window.style.top = this._top + "px";
-    this._window.style.left = this._left + "px";
-    this._window.style.backgroundColor = "#fff";
-    //_window.setAttribute("draggable", "true");
-    this._window.setAttribute("id", this.getID());
-    
-    
-    var closebutton = document.createElement("button");
-    closebutton.classList.add("closebutton");
-    closebutton.appendChild(document.createTextNode("x"));
-    closebutton.addEventListener("click", function(e){
-        JAWM.WindowHandler.destroyWindow(e.target.parentNode.parentNode.id);
-    });
-    var windowicondiv = document.createElement("div"),
-        windowicon = document.createElement("img");
-    windowicondiv.appendChild(windowicon);
-    windowicondiv.style.height = "26px";
-    windowicondiv.style.width = "26px";
-    windowicondiv.style.float = "left";
-    
-    var topbar = document.createElement("div");
-    topbar.classList.add("topbar");
-    topbar.id="topbar" + this.getID();
-    topbar.style.height = "52px";
-    topbar.style.background = "-webkit-linear-gradient(top, #ffffff 0%,#f6f6f6 47%,#ededed 100%)";
-    //topbar.setAttribute("draggable", "true");
-    
-    topbar.appendChild(windowicondiv);
-    topbar.appendChild(closebutton);
-    
-    var bottombar = document.createElement("div");
-    bottombar.classList.add("bottombar");
-    
-    //this._topbar = topbar;
-    this._window.appendChild(topbar);
-    this._window.appendChild(this.content());
-    this._window.appendChild(bottombar);
-    
-    
-    document.getElementById("desktop").appendChild(this._window);
-    this._topbar = document.getElementById("topbar"+this.getID());
-    this._dom = document.getElementById(this.getID());
-    console.log(this._dom);
-    return this;
-};
-Window.prototype.remove = function () {
-    var element = document.getElementById(this.getID());
-    element.parentNode.removeChild(element);
-};
-Window.prototype.move = function (x, y) {
-    this._window.style.left = x + "px";
-    this._window.style.top = y + "px";
-    console.log("hello from move, ", this._window);
-    return this;
-};
-Window.prototype.setzIndex = function (zindex) {
-    this._window.style.zIndex = zindex;
-    this._zindex = zindex;
-    return this;
-};
-Window.prototype.getzIndex = function () {
-    return this._zindex;
-};
+        console.log("id ", id);
+        console.log("_id ", this._id);
+    };
+    var Window = JAWM.Window;
+    Window.prototype.setSize = function (width, height) {
+        console.log("Inside setSize");
+        this._dom.style.height = height + "px";
+        this._dom.style.width = width + "px";
+        console.log(this._dom);
+        this._height = height;
+        this._width = width;
+        return this;
+    };
+    Window.prototype.content = function () {
+        var div = document.createElement("div");
+        div.classList.add("content");
+        return div;
+    };
+    Window.prototype.getWindow = function () {
+        return this._window;
+    };
+    Window.prototype.getTopbar = function () {
+        return this._topbar;
+    };
+    Window.prototype.getID = function () {
+        return this._id;
+    };
+    Window.prototype.getSize = function () {
+        return [this._width, this._height];
+    };
+    Window.prototype.render = function () {
+        this._window = document.createElement("div");
+        this._window.classList.add("icon");
+        this.setzIndex(this.getID());
+        //this._window.style.overflowY = "scroll";
+        this._window.style.width = this._width + "px";
+        //this._window.style.height = this._height + "px";
+        this._window.style.top = this._top + "px";
+        this._window.style.left = this._left + "px";
+        this._window.style.backgroundColor = "#fff";
+        //_window.setAttribute("draggable", "true");
+        this._window.setAttribute("id", this.getID());
+
+
+        var closebutton = document.createElement("button");
+        closebutton.classList.add("closebutton");
+        closebutton.appendChild(document.createTextNode("x"));
+        closebutton.addEventListener("click", function (e) {
+            JAWM.WindowHandler.destroyWindow(e.target.parentNode.parentNode.id);
+        });
+        var windowicondiv = document.createElement("div"),
+            windowicon = document.createElement("img");
+        windowicondiv.appendChild(windowicon);
+        windowicondiv.style.height = "26px";
+        windowicondiv.style.width = "26px";
+        windowicondiv.style.float = "left";
+
+        var topbar = document.createElement("div");
+        topbar.classList.add("topbar");
+        topbar.id = "topbar" + this.getID();
+        topbar.style.height = "52px";
+        topbar.style.background = "-webkit-linear-gradient(top, #ffffff 0%,#f6f6f6 47%,#ededed 100%)";
+        //topbar.setAttribute("draggable", "true");
+
+        topbar.appendChild(windowicondiv);
+        topbar.appendChild(closebutton);
+
+        var bottombar = document.createElement("div");
+        bottombar.classList.add("bottombar");
+
+        //this._topbar = topbar;
+        this._window.appendChild(topbar);
+        this._window.appendChild(this.content());
+        this._window.appendChild(bottombar);
+
+
+        document.getElementById("desktop").appendChild(this._window);
+        this._topbar = document.getElementById("topbar" + this.getID());
+        this._dom = document.getElementById(this.getID());
+        console.log(this._dom);
+        return this;
+    };
+    Window.prototype.remove = function () {
+        var element = document.getElementById(this.getID());
+        element.parentNode.removeChild(element);
+    };
+    Window.prototype.move = function (x, y) {
+        this._window.style.left = x + "px";
+        this._window.style.top = y + "px";
+        console.log("hello from move, ", this._window);
+        return this;
+    };
+    Window.prototype.setzIndex = function (zindex) {
+        this._window.style.zIndex = zindex;
+        this._zindex = zindex;
+        return this;
+    };
+    Window.prototype.getzIndex = function () {
+        return this._zindex;
+    };
+}());

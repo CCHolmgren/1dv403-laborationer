9136afc968581f52f873a12f6098593b768777bf
Christfoffer Holmgren <christoffer.holmgren@gmail.com>
2013-12-28 00:54:34 +0100

    trying to update the content in the taskmanager, but somehow it doesn't want me to when I call the method from inside the object. Odd, will look at it later

--- 5-pwd/TaskManager.js
+++ 5-pwd/TaskManager.js
@@ -7,10 +7,11 @@
         this._left = left;
         this._loadedimages = null;
         this._name = "TaskManager";
+        //this._intervalid = setInterval(this.updatecontent(), 1000);
     };
     JAWM.TaskManager.prototype = new JAWM.Window();
 
-    JAWM.TaskManager.prototype.content = function () {
+    JAWM.TaskManager.prototype.content = function (bottombar) {
         var div = document.createElement("div");
         var windowdiv = document.createElement("div");
         var windows = JAWM.WindowHandler.getWindowObj();
@@ -18,13 +19,24 @@
         var windowlist = keys.map(function(key){
             return windows[key];
         });
-        console.log(windowlist);
+        console.log("windowlist", windowlist);
         windowlist.forEach(function(element){
             var divi = document.createElement("div");
             divi.innerHTML = element._name;
             windowdiv.appendChild(divi);
         });
         div.appendChild(windowdiv);
+        div.id = "content";
+        //setTimeout(JAWM.WindowHandler.getWindow(this._id)[1].updatecontent, 500);
+        console.log(div);
         return div;
+    };
+    JAWM.TaskManager.prototype.updatecontent = function() {
+        var contentdiv = document.getElementById("content");
+        var thiswindow = JAWM.WindowHandler.getWindow(this._id)[1];
+        console.log(contentdiv);
+        console.log("thiswindow.content()", thiswindow.content());
+        contentdiv.innerHTML = thiswindow.content()+"";
+        console.log(contentdiv);
     };
 }());
